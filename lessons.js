7ce0411c2d24e57cda123ac183401caf2d85094a
litmus-paper <nir.eden@gmail.com>
2018-03-29 22:46:08 +0300

    1st commit

---
+++ lessons.js
@@ -0,0 +1,11 @@
+<script type="application/processing" id="000-0"></script>
+
+<script type="application/processing" id="001-0">size(640,400);
+ellipse(320, 200, 300, 300);</script>
+
+<script type="application/processing" id="001-1">size(640,400);
+ellipse(320, 200, 300, 300);
+ellipse(250, 150, 30, 30);
+ellipse(320 - 250 + 320, 150, 30, 30);
+ellipse(320, 270, 90, 40);</script>
+
